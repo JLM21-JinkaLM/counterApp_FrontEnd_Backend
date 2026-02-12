b6b35c338e64767201c0b96eb6b0ababf94b265a
JLM21-JinkaLM <111144226+JLM21-JinkaLM@users.noreply.github.com>
2026-02-12 23:07:03 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import Counter from "./Counter";
+
+function App() {
+  return (
+     <div className="container-fluid min-vh-100 d-flex justify-content-center align-items-center">
+      <div className="col-md-4">
+        <Counter />
+      </div>
+    </div>
+  );
+}
+
+export default App;
